d009ea053cfe4cfcb22d3fd7de3325aa6cbe46a2
romeo198808 <romeo1988.08.03@gmail.com>
2023-03-14 20:29:51 +0200

    1

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var plumber =require('gulp-plumber');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var server =require('browser-sync').create();
+
+gulp.task('style', function(){
+	return gulp.src('src/less/style.less')
+	.pipe(plumber())
+	.pipe(less())
+	.pipe(autoprefixer())
+	.pipe(gulp.dest('src/css'))
+	.pipe(cssmin())
+	.pipe(rename('style.min.css'))
+	.pipe(gulp.dest('src/css'))
+	.pipe(server.stream())
+});
+
+gulp.task('serve', function() {
+	server.init({
+		server: "src",
+		notify: false,
+		open: true,
+		cors: true,
+		ui: false
+	});
+	gulp.watch('src/less/**/*.less', gulp.series('style')).on('change', server.reload);
+	gulp.watch('src/*.html').on('change', server.reload);
+});
+
+gulp.task('start', gulp.series('style', 'serve'));
